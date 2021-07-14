9f1a2325264089f11eadd6d29b8d4aae41feecad
Totsugekitai <37617413+Totsugekitai@users.noreply.github.com>
2021-07-14 20:57:52 +0900

    fix

---
+++ settings.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+PWD=`pwd`
+
+link()
+{
+    if [ ! -f $PWD/$1 ]; then
+        echo -e 'no such file' "\t$1"
+        exit
+    fi
+    if [ ! -h $2 ]; then
+        ln -s $PWD/$1 $2
+        echo '[link]' "$1" 'to' "$2"
+    else
+        echo -e "$2\t" 'is already linked'
+    fi
+}
+
+oh-my-bash_install()
+{
+    if [ -d $HOME/.oh-my-bash ]; then
+        echo 'on-my-bash is already installed'
+    else
+        bash -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)"
+    fi
+}
+
+oh-my-bash_install
+
+link clang-format $HOME/.clang-format
+link init.el $HOME/.emacs.d/init.el
+link tmux.conf $HOME/.tmux.conf
+link bashrc $HOME/.bashrc
